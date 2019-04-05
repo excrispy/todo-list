4604e2c37b0fb061312c3d12ee9a82899c1b5d76
Austin <excrispy@gmail.com>
2019-04-05 17:07:48 -0600

    added drawer and dropdown

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from '../../../node_modules/react';
+import SidePanel from '../side-panel/index';
 import './index.css';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
+        <SidePanel lists={ [{ name: 'Select a task', rows: [] }] }></SidePanel>
       </div>
     );
   }
--- src/containers/side-panel/index.js
+++ src/containers/side-panel/index.js
@@ -1,11 +1,30 @@
+
 import React, { Component } from '../../../node_modules/react';
+import Drawer from '@material-ui/core/Drawer';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 import './index.css';
 
 class SidePanel extends Component {
-    render() {
+  getTasks() {
+    return this.props.lists.map(d => <MenuItem value={ d.name }>{ d.name }</MenuItem> );
+  }
+
+  render() {
     return (
-      <div className="sidePanel">
-      </div>
+        <div>
+          <Drawer
+            open={ true }
+            variant="permanent"
+            anchor="left"
+          >
+            <Select
+              children={ this.getTasks() }
+              value={ this.props.lists[0].name }
+            >
+            </Select>
+          </Drawer>
+        </div>
     );
   }
 }
